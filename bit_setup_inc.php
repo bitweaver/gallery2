b326f1e1c3f6060207f5deb6fb51bbf46cb70d7e
Max Kremmel <xing@synapse.plus.com>
2005-12-18 22:28:52 +0000

    merge recent changes into HEAD

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -3,6 +3,6 @@ global $gBitSystem;
 
 $gBitSystem->registerPackage( 'gallery2', dirname( __FILE__ ).'/' );
 if( $gBitSystem->isPackageActive( 'gallery2' ) ) {
-	$gBitSystem->registerAppMenu( 'gallery2', GALLERY2_PKG_DIR, GALLERY2_PKG_URL.'index.php', 'bitpackage:gallery2/menu_gallery2.tpl', 'gallery2' );
+	$gBitSystem->registerAppMenu( GALLERY2_PKG_DIR, 'Gallery2', GALLERY2_PKG_URL.'index.php', 'bitpackage:gallery2/menu_gallery2.tpl', 'gallery2' );
 }
 ?>
