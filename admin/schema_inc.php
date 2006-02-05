8e4869af13e085999488b4a0f4f397dffcfc7381
Max Kremmel <xing@synapse.plus.com>
2006-02-05 21:17:44 +0000

    remove unused parameters in registerPackageInfo

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -3,8 +3,6 @@ global $gBitInstaller;
 $gBitInstaller->registerPackageInfo( 'gallery2', array(
 	'description' => "Gallery 2 is a very advanced image gallery program, which has a multitude of features including automatic thumbnail creation, image resizing, rotation, captioning and more.",
 	'version' => '2.something',
-	'state' => 'external package',
-	'dependencies' => '',
 	'install' => array(
 		'package' => 'gallery2',
 		'file' => 'install/index.php'
